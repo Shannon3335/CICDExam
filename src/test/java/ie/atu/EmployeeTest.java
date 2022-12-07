91381b93a69aa3a4c73c85ee90de02f7f459045a
G00398070 <shannongf74@gmail.com>
2022-12-07 11:44:23 +0000

    Created name fail test for name shorter than 3 characters.

--- src/test/java/ie/atu/EmployeeTest.java
+++ src/test/java/ie/atu/EmployeeTest.java
@@ -18,6 +18,14 @@ class EmployeeTest {
         Employee Joe = new Employee("Joe",12345678902L,"Full-time","Man",50);
         assertEquals("Joe",Joe.getName());
     }
+
+    @Test
+    void test_fail_name_1()
+    {
+        Employee Jo;
+        Exception e_message = assertThrows(IllegalArgumentException.class,()->new Employee("Jo",12345678902L,"Full-time","Woman",50));
+        assertEquals("Name must be 3 to 25 character long", e_message.getMessage());
+    }
     @AfterAll
     static void afterAll() {
 
